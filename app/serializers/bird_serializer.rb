49a2f406aa57e9264cda0d994089f19451d241e4
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-12-15 22:03:10 +0000

    Automatically backed up by Learn

--- app/serializers/bird_serializer.rb
+++ app/serializers/bird_serializer.rb
@@ -1,4 +1,9 @@
 class BirdSerializer
   include FastJsonapi::ObjectSerializer
+<<<<<<< HEAD
   attributes :name, :species
 end
+=======
+  attributes 
+end
+>>>>>>> 04c0ab70213d1534585cc6fe6d3a2b0e5e506d02
